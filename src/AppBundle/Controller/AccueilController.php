9dce4c914e2d0ae3a6ee8060858ced49456d1b19
theAbasse <abasse.ahmadi@etu.u-bordeaux.fr>
2017-03-16 21:34:25 +0100

    MAJ accueil

--- src/AppBundle/Controller/AccueilController.php
+++ src/AppBundle/Controller/AccueilController.php
@@ -8,8 +8,9 @@ class AccueilController extends Controller
 {
     public function accueilAction()
     {
+        $types = $this->getTypes();
         return $this->render('AppBundle:Accueil:accueil.html.twig', array(
-            // ...
+            'types' => $types,
         ));
     }
 
@@ -18,5 +19,14 @@ class AccueilController extends Controller
         $roles = $this->container->getParameter('security.role_hierarchy.roles');
         return $roles;
     }*/
+
+    private function getTypes(){
+        $types[] = array('libelle' => 'traçabilité', 'root' => 'tracabilite_index', 'icon'=>'fa-barcode');
+        $types[] = array('libelle' => 'temperature', 'root' => 'temperature_index', 'icon'=>'fa-thermometer-empty' );
+        $types[] = array('libelle' => 'reception', 'root' => 'reception_index', 'icon'=>'fa-truck');
+        $types[] = array('libelle' => 'historique', 'root' => 'historique_index', 'icon'=>'fa-hdd-o');
+
+        return $types;
+    }
 
 }
